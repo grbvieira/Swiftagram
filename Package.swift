6a6ea22d5d1cdb517afcce707d156fa39403cab3
Gerson Vieira <gersonbase@gmail.com>
2025-05-08 18:47:01 -0400

    fix could not find a branch named 'composablerequest-7.0.0'

--- Package.swift
+++ Package.swift
@@ -1,46 +1,38 @@
 // swift-tools-version:5.7
 
-import Foundation
 import PackageDescription
-
 
 let package = Package(
     name: "Swiftagram",
-    // Supported versions.
     platforms: [
-        .iOS("13.0"),
-        .macOS("10.15"),
-        .tvOS("13.0"),
-        .watchOS("6.0")
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
-    // Exposed libraries.
     products: [
-        .library(
-            name: "Swiftagram",
-            targets: ["Swiftagram"]
-        ),
-        .library(
-            name: "SwiftagramCrypto",
-            targets: ["SwiftagramCrypto"]
-        )
+        .library(name: "Swiftagram", targets: ["Swiftagram"]),
+        .library(name: "SwiftagramCrypto", targets: ["SwiftagramCrypto"])
     ],
-    // Package dependencies.
     dependencies: [
-        .package(url: "https://github.com/sbertix/ComposableRequest", branch: "composablerequest-7.0.0"),
+        .package(url: "https://github.com/sbertix/ComposableRequest", .upToNextMinor(from: "7.0.0")),
         .package(url: "https://github.com/sbertix/SwCrypt.git", .upToNextMinor(from: "5.1.0"))
     ],
-    // All targets.
     targets: [
         .target(
             name: "Swiftagram",
-            dependencies: [.product(name: "Requests", package: "ComposableRequest"),
-                           .product(name: "Storages", package: "ComposableRequest")]
+            dependencies: [
+                .product(name: "Requests", package: "ComposableRequest"),
+                .product(name: "Storages", package: "ComposableRequest")
+            ]
         ),
         .target(
             name: "SwiftagramCrypto",
-            dependencies: ["Swiftagram",
-                           .product(name: "EncryptedStorages", package: "ComposableRequest"),
-                           .product(name: "SwCrypt", package: "SwCrypt")]
+            dependencies: [
+                "Swiftagram",
+                .product(name: "EncryptedStorages", package: "ComposableRequest"),
+                .product(name: "SwCrypt", package: "SwCrypt")
+            ]
         ),
         .testTarget(
             name: "SwiftagramTests",
@@ -48,8 +40,3 @@ let package = Package(
         )
     ]
 )
-
-if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
-    // #workaround(xcodebuild -version 11.6, Test targets don’t work on watchOS.) @exempt(from: unicode)
-    package.targets.removeAll(where: { $0.isTest })
-}
